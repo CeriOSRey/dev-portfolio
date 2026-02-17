e87e900e064631864fc871c15e503673afdfe8d7
Ceriosly <99903184+CeriOSRey@users.noreply.github.com>
2026-02-16 21:45:47 -0800

    tiny commit to test vercel link

--- backend-server-do-not-use/seed.mjs
+++ backend-server-do-not-use/seed.mjs
@@ -1,7 +1,7 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import bcrypt from 'bcryptjs';
-
+// This script seeds the SQLite database with demo users and their associated data.
 async function seed() {
   const db = await open({ filename: './server/app.db', driver: sqlite3.Database });
   await db.exec(`
